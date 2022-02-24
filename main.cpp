954f4a2e94471d6afac0a017fa882da308487cf1
Nclose72 <95373223+Nclose72@users.noreply.github.com>
2022-02-24 15:05:09 -0500

    Add files via upload
    First test code

--- main.cpp
+++ main.cpp
@@ -1,116 +1,115 @@
-
 #include <FEHLCD.h>
-#include <FEHIO.h>
-#include <FEHUtility.h>
-#include <FEHMotor.h>
 #include <FEHServo.h>
-#include <FEHAccel.h>
-#include <FEHBattery.h>
-#include <FEHBuzzer.h>
-#include <FEHRPS.h>
-#include <FEHSD.h>
-#include <string.h>
-#include <stdio.h>
-
-#define LEFTFORWARD -25
-#define LEFTBACKWARD 25
-#define RIGHTFORWARD 25
-#define RIGHTBACKWARD -25
-
-FEHMotor LeftIGWAN(FEHMotor::Motor1, 9);
-FEHMotor RightIGWAN(FEHMotor::Motor0, 9);
-
-AnalogInputPin cds(FEHIO::P2_0);
-
-void forward();
-
-void backward();
-
-void left();
-
-void right();
-
-void stop();
-
-void forward()
-{
-    LeftIGWAN.SetPercent(LEFTFORWARD);
-    RightIGWAN.SetPercent(RIGHTFORWARD);
-}
-
-void backward()
-{
-    LeftIGWAN.SetPercent(LEFTBACKWARD);
-    RightIGWAN.SetPercent(RIGHTBACKWARD);
-}
-
-void right()
-{
-    LeftIGWAN.SetPercent(LEFTBACKWARD);
-    RightIGWAN.SetPercent(RIGHTFORWARD);
-}
-
-void left()
-{
-    LeftIGWAN.SetPercent(LEFTFORWARD);
-    RightIGWAN.SetPercent(RIGHTBACKWARD);
-}
-
-void stop()
-{
-    LeftIGWAN.Stop();
-    RightIGWAN.Stop();
-}
-
+#include <FEHMotor.h>
+#include <FEHUtility.h>
+#include <FEHIO.h>
+//Declare 2 igwan motors to motor ports
+FEHMotor left_motor(FEHMotor::Motor0,9.0);  
+FEHMotor right_motor(FEHMotor::Motor1,9.0);  
+DigitalEncoder right_encoder(FEHIO::P1_0);
+DigitalEncoder left_encoder(FEHIO::P1_1);
 int main(void)
 {
-    //move forward from welcome mat
+move_forward(25,405);
+turn_left(25,);
+}
+
+//Function that calculates average voltage value of CDS cell 0-3.3 V
+int lightValue(){
+    //Declare input to cds cell
+AnalogInputPin cds(FEHIO::P0_0);
+int i=0;
+float sum=0;
+float a=0;
+float average=0;
+//Loop runs through 10 times to measure cds cell output
+while(i<10){
+    //Find value and add it to running sum
+     a= cds.Value();
+     sum=sum+a;
+     i++;
+     //Sleep 10 ms
+     Sleep(10);
+  
+
+
+}
+//calculate average value and return said value
+
+average=sum/10;
+return average;
+
+}
+
+void forward(){ 
     
-    forward();
-
-    //stop at turning point
-
-    // **** Shaft Encoder Code ***
-
-    stop();
-
-    Sleep(1.0);
-
-    //turn to face jukebox
-
-    right();
-
-    //stop when facing juke box
-
-    // **** Shaft Encoder Code ***
-
-    stop();
-
-    //move to light
-
-    forward();
-
-    //stop on light
-
-    // **** Shaft Encoder Code ***
-
-    stop();
-
-    //read, interpret, and turn right/left depending on light
-
-    if (cds.Value < )
-    {
-
-    }
-    else if(cds.Value > )
-    {
-
-    }
-    else
-    {
-
-    }
+   left_motor.SetPercent(25);
+   right_motor.SetPercent(25);
 
 
+}
+void turnLeft(){ 
+    
+   left_motor.SetPercent(0);
+   right_motor.SetPercent(25);
+
+
+}
+void turnRight(){ 
+    
+   left_motor.SetPercent(25);
+   right_motor.SetPercent(0);
+
+
+}
+void move_forward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+void turn_left(int percent, int counts)
+{
+     //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while(right_encoder.Counts() < counts);
+    //Turn off motors
+    right_motor.Stop();
+
+}
+
+void turn_right(int percent, int counts)
+{
+     //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    left_motor.SetPercent(percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while(left_encoder.Counts() < counts);
+
+    //Turn off motors
+    left_motor.Stop();
 
 }
